e598d802be7e8d003023756f1e8c162a8710d44e
Mohammadaasif1 <asif00077786@gmail.com>
2024-02-11 12:59:55 +0530

    add bombArray, emptyArray,randomArray and use sort method to with random()

--- Mohammad_Aasif/script.js
+++ Mohammad_Aasif/script.js
@@ -1,12 +1,24 @@
 
 const gridContent = document.querySelector(".grid-content");
-const bombCell = 20;
+const flagsLeft = document.querySelector("#flagsLeft");
+let bombCell = 20;
 
   function gridGameBoard(rows, cols) {
+
+    flagsLeft.innerHTML = bombCell;
+
+    const bombArray = Array(bombCell).fill("bomb");
+    const emptyArray = Array((rows * cols) - bombCell).fill("valid");
+    const concatArray = emptyArray.concat(bombArray);
+    const randomArray = concatArray.sort(() => Math.random() - 0.5);
+    console.log(randomArray);
+    
+    
+
     for (let i = 0; i < rows * cols; i++) {
       const gridCell = document.createElement("div");
       gridCell.id = "cell-"+ i;
-      
+      gridCell.classList.add(randomArray[i]);
       gridContent.appendChild(gridCell);
     }
   }
